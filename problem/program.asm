243e055b1925bd9ddbb48e7107ab7dfbdd5ac39f
andruAJ <87772588+andruAJ@users.noreply.github.com>
2022-02-13 21:52:37 -0500

    Lo siento, ya puse el código aquí

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,59 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
-(START)
-    @START
-    0;JMP
+//AndresJuanGiraldoVargas
+//000393367
+//andres.giraldov@upb.edu.co
+
+(CICLO)
+@SCREEN
+D=A
+@0
+M=D
+
+(TECLADO)
+
+@KBD 
+D=M 
+@70
+D=D-A
+@FILL
+D;JEQ
+//hasta arriba funciona perfectamente
+@KBD
+D=M
+@67
+D=D-A
+@CLEAN
+D;JEQ
+//Mantiene el ciclo en caso de que no se hunda nada
+@TECLADO
+0;JMP
+
+(FILL)
+@1
+M=-1
+@CHANGE
+0;JMP
+
+(CLEAN)
+@1
+M=0
+@CHANGE
+0;JMP
+
+(CHANGE)
+@1
+D=M
+@0
+A=M
+M=D
+@0
+D=M+1
+@KBD
+D=A-D
+@0
+M=M+1
+A=M
+@CHANGE
+D;JGT
+
+@CICLO
+0;JMP
